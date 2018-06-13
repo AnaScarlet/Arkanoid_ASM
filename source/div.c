61b1620f9147cecbed6f3ffcf34ad2fc16626c98
ummeyt <35542644+ummeyt@users.noreply.github.com>
2018-06-13 13:13:34 -0600

    Update div.c

--- source/div.c
+++ source/div.c
@@ -1,19 +1,9 @@
 #include <stdio.h>
 
-int main()
-{
-  divFuncX(800);
-  divFuncY(800);
-
-  return 0;
-}
-
 int divFuncX (int x)
 {
   x -= 612;
   x /= 60;
-
-  printf("x is %d\n", x);
   
   return x;
 }
@@ -22,8 +12,6 @@ int divFuncY (int y)
 {
   y -=127;
   y /= 40;
-
-  printf("y is %d\n", y);
   
   return y;
 }
